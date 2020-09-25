0521ade5e6f4472389618de8863add1eefd8567a
pragyaaaaa <pragyasabstracts@gmail.com>
2020-09-25 22:05:57 +0530

    optimized imports.

--- LawOfLargeNumbers/LawOfLargeNumbers.py
+++ LawOfLargeNumbers/LawOfLargeNumbers.py
@@ -1,6 +1,5 @@
 # The law of large numbers, in probability and statistics, states that as a sample size grows, it's mean gets closer
 # to the average of the whole population.
-import numpy as np
 from numpy.random import randn
 
 
