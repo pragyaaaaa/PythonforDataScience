a0d636999aa6aafdb9683528ce37beba56ab96d2
pragyaaaaa <pragyasabstracts@gmail.com>
2020-09-25 21:52:41 +0530

    Law of large numbers solution updated.

--- LawOfLargeNumbers/LawOfLargeNumbers.py
+++ LawOfLargeNumbers/LawOfLargeNumbers.py
@@ -3,11 +3,16 @@
 import numpy as np
 from numpy.random import randn
 
+
+def law_of_large_numbers(num):
+    counter = 0
+    for i in randn(num):
+        if -1 < i < 1:
+            counter += 1
+    hit_ratio = counter / num
+    hit_ratio_per = hit_ratio * 100
+    print(f"{hit_ratio} i.e. {hit_ratio_per}% numbers fall between -1 and 1 for {num}.")
+
+
 n = int(input("How many numbers you want to generate?:\t"))
-counter = 0
-for i in randn(n):
-    if -1 < i < 1:
-        counter += 1
-hit_ratio = counter / n
-hit_ratio_per=hit_ratio*100
-print(f"{hit_ratio} i.e. {hit_ratio_per}% numbers fall between -1 and 1 for {n}.")
+law_of_large_numbers(n)
