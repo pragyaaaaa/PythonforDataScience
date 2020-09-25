00d680d070dcdbceee55bf0dacf0c7312ab7f385
pragyaaaaa <pragyasabstracts@gmail.com>
2020-09-25 21:59:41 +0530

    Law of large numbers solution updated.

--- LawOfLargeNumbers/LawOfLargeNumbers.py
+++ LawOfLargeNumbers/LawOfLargeNumbers.py
@@ -11,8 +11,22 @@ def law_of_large_numbers(num):
             counter += 1
     hit_ratio = counter / num
     hit_ratio_per = hit_ratio * 100
-    print(f"{hit_ratio} i.e. {hit_ratio_per}% numbers fall between -1 and 1 for {num}.")
+    # print(f"{hit_ratio} i.e. {hit_ratio_per}% numbers fall between -1 and 1 for {num}.")
+    return hit_ratio_per
 
 
-n = int(input("How many numbers you want to generate?:\t"))
-law_of_large_numbers(n)
+def input_for_lln():
+    n = int(input("How many numbers you want to generate?:\t"))
+    law_of_large_numbers(n)
+    return n
+
+
+def compare_for_inputs(input_1, input_2):
+    h_r_ip1 = law_of_large_numbers(input_1)
+    h_r_ip2 = law_of_large_numbers(input_2)
+    print(f"For {input_1}, {h_r_ip1}% numbers fit criteria, where as for {input_2}, {h_r_ip2}% numbers do.")
+
+
+in_1 = input_for_lln()
+in_2 = input_for_lln()
+compare_for_inputs(in_1, in_2)
