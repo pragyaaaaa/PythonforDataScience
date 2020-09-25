eac700b54ef5eed90fadb84f0c4622e8d5b4e230
pragyaaaaa <pragyasabstracts@gmail.com>
2020-09-25 21:48:00 +0530

    Law of large numbers solution implemented.

--- LawOfLargeNumbers/LawOfLargeNumbers.py
+++ LawOfLargeNumbers/LawOfLargeNumbers.py
@@ -1,2 +1,12 @@
 # The law of large numbers, in probability and statistics, states that as a sample size grows, it's mean gets closer
 # to the average of the whole population.
+import numpy as np
+from numpy.random import randn
+
+n = input("How many numbers you want to generate?:\t")
+counter = 0
+for i in randn(n):
+    if -1 < i < 1:
+        counter += 1
+hit_ratio = counter / n
+print(f"{hit_ratio} numbers fall between -1 and 1 for {n}.")
