db787f0c5a3670c653b0276936bac5a850bd708f
pragyaaaaa <pragyasabstracts@gmail.com>
2020-09-27 00:02:10 +0530

    Basketball free throws dataset visualized: using functions updated.

--- BasketballFreeThrows/VisualizationFunction.py
+++ BasketballFreeThrows/VisualizationFunction.py
@@ -8,12 +8,22 @@ players = d.Players
 players_dict = d.Pdict
 
 
-def player_plot_salary(player_dict, color='Black', line_style=':', mark='s'):
+def player_plot_salary(player_dict):
+    color = {"KobeBryant": 'Red', "JoeJohnson": "gold", "LeBronJames": "darkorchid", "CarmeloAnthony": "teal",
+             "DwightHoward": "cyan", "ChrisBosh": "crimson",
+             "ChrisPaul": "orchid", "KevinDurant": "green", "DerrickRose": "salmon", "DwayneWade": "black"}
+    line_style = {"KobeBryant": ":", "JoeJohnson": "-.", "LeBronJames": "-", "CarmeloAnthony": "--",
+                  "DwightHoward": ":", "ChrisBosh": "-.",
+                  "ChrisPaul": "-", "KevinDurant": "--", "DerrickRose": ":", "DwayneWade": "-."}
+    mark = {"KobeBryant": "o", "JoeJohnson": "v", "LeBronJames": "<", "CarmeloAnthony": "P", "DwightHoward": "D",
+            "ChrisBosh": "X",
+            "ChrisPaul": "H", "KevinDurant": "8", "DerrickRose": ">", "DwayneWade": "s"}
     for name in player_dict:
-        plt.plot(salary[player_dict[name]], c=color, ls=line_style, marker=mark, ms=7, label=players[player_dict[name]])
+        plt.plot(salary[player_dict[name]], c=color[name], ls=line_style[name], marker=mark[name], ms=7,
+                 label=players[player_dict[name]])
     plt.legend(loc='upper left', bbox_to_anchor=(1, 1))
     plt.xticks(list(range(0, 10)), seasons, rotation='vertical')
     plt.show()
 
 
-player_plot_salary(players_dict, 'red', '--', 'o')
+player_plot_salary(players_dict)
