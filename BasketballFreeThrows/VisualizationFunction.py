2bbf120e86646e9eabf69d9be0fd5a41b5907bd3
pragyaaaaa <pragyasabstracts@gmail.com>
2020-09-27 00:06:47 +0530

    Basketball free throws dataset visualized: using functions updated- default params added.

--- BasketballFreeThrows/VisualizationFunction.py
+++ BasketballFreeThrows/VisualizationFunction.py
@@ -5,10 +5,11 @@ from DataSets import BasketballDataSet as d
 salary = d.Salary
 seasons = d.Seasons
 players = d.Players
+games = d.Games
 players_dict = d.Pdict
 
 
-def player_plot_salary(player_dict):
+def player_plot(data_matrix=salary, player_dict=players_dict):
     color = {"KobeBryant": 'Red', "JoeJohnson": "gold", "LeBronJames": "darkorchid", "CarmeloAnthony": "teal",
              "DwightHoward": "cyan", "ChrisBosh": "crimson",
              "ChrisPaul": "orchid", "KevinDurant": "green", "DerrickRose": "salmon", "DwayneWade": "black"}
@@ -19,11 +20,12 @@ def player_plot_salary(player_dict):
             "ChrisBosh": "X",
             "ChrisPaul": "H", "KevinDurant": "8", "DerrickRose": ">", "DwayneWade": "s"}
     for name in player_dict:
-        plt.plot(salary[player_dict[name]], c=color[name], ls=line_style[name], marker=mark[name], ms=7,
+        plt.plot(data_matrix[player_dict[name]], c=color[name], ls=line_style[name], marker=mark[name], ms=7,
                  label=players[player_dict[name]])
     plt.legend(loc='upper left', bbox_to_anchor=(1, 1))
     plt.xticks(list(range(0, 10)), seasons, rotation='vertical')
     plt.show()
 
 
-player_plot_salary(players_dict)
+player_plot(games, players_dict)
+player_plot(salary, players_dict)
