9c4d78ef3e28b697b027e6cbdf809c8abbb96437
pragyaaaaa <pragyasabstracts@gmail.com>
2020-10-01 08:11:38 +0530

    World trends statement: lmplot.

--- WorldTrends/WorldTrends.py
+++ WorldTrends/WorldTrends.py
@@ -8,5 +8,8 @@ warnings.filterwarnings('ignore')
 stats = pd.read_csv("..\\DataSets\\P4-Demographic-Data.csv")
 stats.columns = ['CountryName', 'CountryCode', 'BirthRate', 'InternetUsers',
                  'IncomeGroup']
-vis1 = sns.distplot(stats["InternetUsers"])
+# xaxis = input("Enter x axis: \t")
+# yaxis = input("Enter y-axis: \t")
+# vis1 = sns.boxplot(data=stats, x=xaxis, y=yaxis)
+vis = sns.lmplot(data=stats, x="InternetUsers", y='BirthRate', fit_reg=False, hue="IncomeGroup")
 plt.show()
